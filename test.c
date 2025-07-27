c35974bbc9234fc08f80b179e106d6e918af7e79
VanshSharma5 <vanshsharmamzn34@gmail.com>
2025-07-27 17:52:27 +0530

    add the test to check

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
+#include<stdio.h>
 #include"features/io.h"
 
 int main() {
-    Int a = int_input("Enter a number :");
-    Double b = double_input("Enter a number :");
-    Float c = float_input("Enter a number :");
-    printf("%d\t%lf\t%f", *a, *b, *c);
+    String name = input("Enter Your Name :<MAX-16>", 16);
+    Int age = inputInt("Enter Your Age :");
+    String gender = input("Enter Your Gender<Male/Female", 8);
+
+    println("The Details You Give Are");
+    print(name);
+    printInt(age);
+    print(gender);
+
+    return 0;
 }
